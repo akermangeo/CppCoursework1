19e6a6273e70ce804566be27fe489b0cd23a6977
Stratisfy <92597913+Stratisfy@users.noreply.github.com>
2022-02-10 11:24:28 +0000

    Add files via upload

--- Q2_solution/loop_test/loop_test.c
+++ Q2_solution/loop_test/loop_test.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, count = 0;
+	for (i = 10; i <= 10; i++)
+	{
+		count++;
+	}
+	printf("%d\n", count);
+	count = 0;
+	for (i = 10; i < 10; i++)
+	{
+		count++;
+	}
+	printf("%d\n", count);
+	count = 0;
+	for (i = 10; i >= 1; i -= 4)
+	{
+		count++;
+	}
+	printf("%d\n", count);
+	count = 0;
+	for (i = 10; i == 1; i -= 4)
+	{
+		count++;
+	}
+	printf("%d\n", count);
+	return EXIT_SUCCESS;
+}
 
