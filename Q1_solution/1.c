8a98e6d044ec781b1dbd9e4552a27ab00d07eb46
Tianhao Wang <46406944+PINGXIANG@users.noreply.github.com>
2022-02-11 15:32:16 +0000

    Add files via upload

---
+++ Q1_solution/1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   
+
+    char char1;
+    char char2;
+    printf("Input char: ");
+    scanf("%c %c", &char1, &char2);
+    printf("Output char:%c %c \n", char2, char1);
+
+
+    return EXIT_SUCCESS;
+}
